3d30c8afcc3cc611099873f05048c372c64490e0
Sanam Limbu <sudosanam@gmail.com>
2025-03-02 12:23:37 +0800

    added new projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,45 +1,45 @@
-import { Box, Typography } from "@mui/material";
-import MyWorkOuts from "./MyWorkouts";
-import RentHome from "./RentHome";
+import { Box, Typography } from '@mui/material';
+import ManUnited from './ManUnited';
+import Mig from './Mig';
 export default function Projects({ id }: { id: string }) {
   return (
     <Box
       id={id}
       sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#f9f9f9",
-        width: "100%",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#f9f9f9',
+        width: '100%',
       }}
     >
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          maxWidth: "62em",
-          padding: "6em 2em",
-          width: "100%",
-          gap: "3em",
+          display: 'flex',
+          flexDirection: 'column',
+          maxWidth: '62em',
+          padding: '6em 2em',
+          width: '100%',
+          gap: '3em',
         }}
       >
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            "@media (max-width: 971px)": {
-              justifyContent: "center",
-              textAlign: "center",
+            display: 'flex',
+            justifyContent: 'space-between',
+            '@media (max-width: 971px)': {
+              justifyContent: 'center',
+              textAlign: 'center',
             },
           }}
         >
           <Box>
             <Typography
               sx={{
-                fontWeight: "800",
-                background: "linear-gradient(to right, #007FFF, #0059B2)",
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent",
+                fontWeight: '800',
+                background: 'linear-gradient(to right, #007FFF, #0059B2)',
+                WebkitBackgroundClip: 'text',
+                WebkitTextFillColor: 'transparent',
               }}
             >
               PORTFOLIO
@@ -47,8 +47,8 @@ export default function Projects({ id }: { id: string }) {
             <Typography
               variant="h5"
               sx={{
-                marginTop: "0.5em",
-                fontWeight: "bold",
+                marginTop: '0.5em',
+                fontWeight: 'bold',
               }}
             >
               Each project is a unique piece of development 🧩
@@ -59,18 +59,18 @@ export default function Projects({ id }: { id: string }) {
 
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            flexWrap: "wrap",
-            gap: "2em",
-            "@media (max-width: 971px)": {
-              justifyContent: "center",
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            flexWrap: 'wrap',
+            gap: '2em',
+            '@media (max-width: 971px)': {
+              justifyContent: 'center',
             },
           }}
         >
-          <MyWorkOuts />
-          <RentHome />
+          <Mig />
+          <ManUnited />
         </Box>
       </Box>
     </Box>
